eba68e405d74a8577a6689a8a39a2cda14cf8f84
femi <cocofemi@gmail.com>
2025-06-07 20:18:56 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ChatBot from "./components/ChatBot";
 
 const App = () => {
   useEffect(() => {
-    localStorage.clear("chatbotChatHistory");
+    localStorage.removeItem("chatbotChatHistory");
   });
   return (
     <>
